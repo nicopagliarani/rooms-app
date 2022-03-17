2c0cb0ded7e32baa76608daaa40979fe194a068e
nicopagliarani <98600157+nicopagliarani@users.noreply.github.com>
2022-03-17 18:23:18 +0100

    first commit

---
+++ models/User.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  { email: String, 
+    password: String, 
+    username: String 
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
